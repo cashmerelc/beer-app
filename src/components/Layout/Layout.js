24dd3bcfdce406153a47cc93fbdacc9039409dd5
Derek Myers <cashmerelc@gmail.com>
2024-06-20 07:32:55 +0200

    Add styling to the beer battle dashboard

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -106,7 +106,7 @@ export default function Layout({ children }) {
         </NavLinks>
       </Navbar>
       <main>{children}</main>
-      <Footer />
+      {session ? <Footer /> : <></>}
     </>
   );
 }
